f08f73bad330ce045f49c97a783523427ca5395f
Oleg Nurzhanov <oleg@nastyle.pro>
2024-01-29 21:03:26 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.png';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  let [playstate,setplaystate] = useState('running')
+  function animation(){
+    setplaystate((playstate === 'running') ? 'paused' : 'running')
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>VueТылочка</p>
+        <div id="vue">
+          <img src={logo} className="App-logo" alt="logo" style={{animationPlayState: playstate}} />
+        </div>
+        <p class="textVue">VueТылочка</p>
         <p>И совсем это не React....</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={animation}>Крутить <span>VueТылочку</span></button>
+        <p class="rub">-50 рублей</p>
       </header>
     </div>
   );
+}
+
+
+function Animation() {
+  let pause = "animation-play-state: paused;"
+  let play = "animation-play-state: running;"
+  document.querySelector(".App-logo").style = ''
+
+  if(document.querySelector(".App-logo").style === play) {
+    document.querySelector(".App-logo").style = pause
+  } else {
+    document.querySelector(".App-logo").style = play
+  }
 }
 
 export default App;
